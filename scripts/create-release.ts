068b403c1c4f8187119ab37a1ebbe2eab1a845f2
Eric Biewener <eric.biewener0@walmart.com>
2022-01-31 17:17:42 -0800

    more - 5:17 pm - Jan 31

--- scripts/create-release.ts
+++ scripts/create-release.ts
@@ -1,42 +1,57 @@
 import compareVersions from "compare-versions";
+import inquirer from "inquirer";
 import open from "open";
 import yargs from "yargs";
 import "zx/globals";
+import manifest from "../manifest.json";
 
 // @ts-ignore
 const { tag } = yargs.option("tag", {
 	alias: "t",
 	describe: "new tag/version to create",
-	required: true,
 }).argv;
 
 const ROOT = path.join(__dirname, "..");
+const MANIFEST_FILENAME = path.join(ROOT, "manifest.json");
+const VERSIONS_FILENAME = path.join(ROOT, "versions.json");
+
+const getVersion = async () => {
+	const { releaseType } = await inquirer.prompt([
+		{
+			message: "Type of release",
+			name: "releaseType",
+			type: "list",
+			choices: ["major", "minor", "patch"],
+		},
+	]);
+
+	console.info("releaseType", releaseType);
+};
 
 const writeManifest = () => {
-	const manifestFilename = path.join(ROOT, "manifest.json");
-	const manifest = require(manifestFilename);
 	manifest.version = tag;
 	return fs.writeFile(
-		manifestFilename,
+		MANIFEST_FILENAME,
 		JSON.stringify(manifest, null, 2),
 		"utf-8"
 	);
 };
 
 const writeVersions = () => {
-	const versionsFilename = path.join(ROOT, "versions.json");
-	const versions = require(versionsFilename);
+	const versions = require(VERSIONS_FILENAME);
 	const appVersions: string[] = Object.values(versions);
 	appVersions.sort(compareVersions);
 	versions[tag] = appVersions[appVersions.length - 1];
 	return fs.writeFile(
-		versionsFilename,
+		VERSIONS_FILENAME,
 		JSON.stringify(versions, null, 2),
 		"utf-8"
 	);
 };
 
 const main = async () => {
+	await getVersion();
+	return;
 	await Promise.all([writeManifest(), writeVersions()]);
 	await $`git add .`;
 	await $`git commit -m "bump version"`;
